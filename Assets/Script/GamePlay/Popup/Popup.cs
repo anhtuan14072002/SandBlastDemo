58105870cef57e6fe8d2dfebe778a45f0594b9b4
TUNAPC <anhtuan14072002@gmail.com>
2025-11-28 17:14:29 +0700

    thêm damage number

---
+++ Assets/Script/GamePlay/Popup/Popup.cs
@@ -0,0 +1,35 @@
+﻿using PrimeTween;
+using UnityEngine;
+
+namespace Sand
+{
+    public class Popup : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Debug.Log("Enable Popup");
+            TweenPopupOpen();
+        }
+
+        private void OnDisable()
+        {
+            gameObject.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+        }
+
+        public void ClosePopup()
+        {
+            TweenPopupClose();
+        }
+
+        private void TweenPopupOpen()
+        {
+            Tween.Scale(gameObject.transform, Vector3.one, 0.25f, Ease.OutBack);
+        }
+
+        private void TweenPopupClose()
+        {
+            Tween.Scale(gameObject.transform, Vector3.zero, 0.25f, Ease.InBack)
+                .OnComplete(() => { gameObject.SetActive(false); });
+        }
+    }
+}
